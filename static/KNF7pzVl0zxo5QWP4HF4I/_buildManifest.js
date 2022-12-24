38b232f238efbcd471b975d26b89d798440f3fbb
wjyflh <lk11260@gmail.com>
2022-12-24 07:42:50 +0000

    Deploying to gh-pages from @ wjyflh/wjy@97289420a0ddd03d0ba1f8f9f2b648b06674e2c5 🚀

--- static/KNF7pzVl0zxo5QWP4HF4I/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/130-4962be6f52908824.js","static/chunks/pages/index-78312db485cb4d70.js"],"/_error":["static/chunks/pages/_error-47e526b08b70a03d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
