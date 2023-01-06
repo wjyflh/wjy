99c611bd14de24a1c23e71e1a288ab13ec3f8863
Jiang <lk11260@gmail.com>
2023-01-06 10:58:05 +0800

    dev (unoptimize next/image)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const isProduction = process.env.BUILD_ENV === "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   assetPrefix: isProduction ? '/wjy/' : undefined,
 }
 
