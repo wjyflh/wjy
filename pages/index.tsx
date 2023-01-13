7283677b94d54a36d44b67152063f800493706ef
Jiang <lk11260@gmail.com>
2023-01-14 00:53:27 +0800

    dev (updated metas)

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,13 +13,27 @@ export default function Home() {
     <MainLayout>
       <Head>
         <title>WJY | Jiang</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="title" content="WJY | Jiang" />
+        <meta name="description" content="I'm Jiang. A senior frontend guy, with music in mind. Jiang,WJY" />
+        <meta name="keywords" content="Jiang,WJY"></meta>
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="WJY | Jiang" />
+        <meta name="twitter:description" content="I'm Jiang. A senior frontend guy, with music in mind." />
+        <meta name="twitter:url" content="https://www.wjy.rocks" />
+
+        <meta property="og:type" content="website" />
         <meta property="og:title" content="WJY | Jiang" />
         <meta property="og:site_name" content="WJY | Jiang" />
-        <meta property="og:url" content="https://www.wjy.rocks/" />
+        <meta property="url" content="https://www.wjy.rocks" />
+        <meta property="og:url" content="https://www.wjy.rocks" />
         <meta property="og:description" content="I'm Jiang. A senior frontend guy, with music in mind." />
-        <meta property="og:type" content="website" />
         <meta property="og:image" content="/wjy_logo.jpg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:image:width" content="512" />
+        <meta property="og:image:height " content="612"></meta>
+
+        <link rel="canonical" href="https://www.wjy.rocks" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
